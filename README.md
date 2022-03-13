3d1ff485d18c5a38049307ff26c655784b845791
enzom07 <enzo.maragliano@gmail.com>
2022-03-13 13:37:55 -0300

    Edit  index.html

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Library
+
+    books = document.querySelectorAll(".container > div");
+    buttons_delete = document.querySelectorAll(".container > div > button");
+    buttons_delete.forEach((button) => {
+
+      console.log(index);
+      button.addEventListener('click', () => {
+    
+        index = (button.className).substr(-1);
+        console.log(index);
+        
+        //console.log(div);
+        //Remove book from Array myLibrary
+        for(let i=0;i<myLibrary.length;i++){
+    
+          if(myLibrary[i].index === index){
+    
+            myLibrary.splice(index,index+1);
+    
+          }
+    
+        }
+        //Remove book from HTML
+        div = document.querySelector('.book'+index);
+        div.remove();
+        //console.log(myLibrary);
+    
+      });
+    
+    });
--- script.js
+++ script.js
@@ -1,155 +1,218 @@
 
 var container = document.querySelector('.container');
-var btn_add = document.querySelector('#btn_add');
-var buttons_delete;
-var books;
 var title;
 var author;
 var pages;
 var read;
 var div;
 var newbook;
-var button;
 var index;
-
 
 
 
 
 var myLibrary = [
 
-    {
-        title:"El Principito",
-        author:"Antoine De Saint-exupéry",
-        pages:"96",
-        read:"not read"
-    },
-    {
-        title:"El Principito2",
-        author:"Antoine De Saint-exupéry",
-        pages:"96",
-        read:"not read"
-    },
-    {
-      title:"El Principito3",
+  {
+      title:"El Principito",
       author:"Antoine De Saint-exupéry",
       pages:"96",
-      read:"not read"
-  }
+      read:"yes",
+      index:0
+  },
+  {
+      title:"El Principito2",
+      author:"Antoine De Saint-exupéry",
+      pages:"96",
+      read:"no",
+      index:1
+  },
+  {
+    title:"El Principito3",
+    author:"Antoine De Saint-exupéry",
+    pages:"96",
+    read:"no",
+    index:2
+}
 
 ];
 
-function Book(title,author,pages,read) {
+class Book{
 
+  constructor(title,author,pages,read,ind){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-   // console.log(this);
-
+    this.index = ind;
+  }
 }
 
-
-  //button Add Book
-btn_add.addEventListener('click', () => {
- 
-  let form = document.getElementById('form');
-
-  //title = document.querySelector('#title').value;
-  title = (form.elements[0]).value;
-  console.log(title);
-  newbook = new Book(title,author,pages,read);
-  //Call to display info books
- /* books = document.querySelectorAll(".container > div");
-  addBookToLibrary(newbook);
-  delete_grid();
-  info(myLibrary); */
+function EditStatus(value) {
   
-});
+  this.read = value;
 
-//Addbook to Array
-function addBookToLibrary(book) {
-
-    myLibrary.push(book);
-    
 }
 
-//addBookToLibrary.prototype = Object.create(Book.prototype);
+EditStatus.prototype = Object.create(Book.prototype);
+
 
 function inicial(myLibrary){
 
-    //console.log(myLibrary.length);
-    for(i=0;i<myLibrary.length;i++){
+  //console.log(myLibrary.length);
+  for(let i=0;i<myLibrary.length;i++){
 
-        
-        //Add to view a book
-        div = document.createElement('div');
-        div.classList.add('book'+[i]); 
-        container.appendChild(div);
-        
+      
+      //Add to view a book
+      div = document.createElement('div');
+      div.classList.add('book'+myLibrary[i].index);
+      container.appendChild(div);
+      
 
 
-        //Add Info about book
-        divinfo = document.createElement('div');
-        divinfo.classList.add('title:'+i);
-        divinfo.textContent = "Title:"+myLibrary[i].title;
-        div.appendChild(divinfo); 
+      //Add Info about book
+      divinfo = document.createElement('div');
+      divinfo.classList.add('title'+myLibrary[i].index);
+      divinfo.textContent = "Title:"+myLibrary[i].title;
+      div.appendChild(divinfo); 
 
-        divinfo = document.createElement('div');
-        divinfo.classList.add('author:'+i);
-        divinfo.textContent = "Author:"+myLibrary[i].author;
-        div.appendChild(divinfo); 
+      divinfo = document.createElement('div');
+      divinfo.classList.add('author'+myLibrary[i].index);
+      divinfo.textContent = "Author:"+myLibrary[i].author;
+      div.appendChild(divinfo); 
 
-        divinfo = document.createElement('div');
-        divinfo.classList.add('pages:'+i);
-        divinfo.textContent = "Pages:"+myLibrary[i].pages;
-        div.appendChild(divinfo); 
+      divinfo = document.createElement('div');
+      divinfo.classList.add('pages'+myLibrary[i].index);
+      divinfo.textContent = "Pages:"+myLibrary[i].pages;
+      div.appendChild(divinfo); 
 
-        divinfo = document.createElement('div');
-        divinfo.classList.add('read:'+i);
-        divinfo.textContent = "Read:"+myLibrary[i].read;
-        div.appendChild(divinfo);
-        
+      divinfo = document.createElement('div');
+      divinfo.classList.add('read'+myLibrary[i].index);
+      divinfo.textContent = "Read:"+myLibrary[i].read;
+      div.appendChild(divinfo);
+      
+      //Button Delate Book
+      button = document. createElement('button');
+      button.type = 'button';
+      button.classList.add('button'+myLibrary[i].index);
+      button.setAttribute("id", "book"+myLibrary[i].index); 
+      button.innerText = 'Delete';
+      div.appendChild(button);
 
-        button = document. createElement('button');
-        button.type = 'button';
-        button.classList.add('button'+i);
-        button.innerText = 'Delete';
-        div.appendChild(button);
+      //Button Edit Book
+      button = document. createElement('button');
+      button.type = 'button';
+      button.setAttribute("id", "edit"+myLibrary[i].index); 
+      button.innerText = 'Edit Status';
+      div.appendChild(button);
 
-    }
+  }
 
 }
+
+//Addbook to Array and HTML
+function addBookToLibrary() {
+
+  let max =0;
+  title = document.querySelector('#title').value;
+  author = document.querySelector('#author').value;
+  pages = document.querySelector('#pages').value;
+  read = document.querySelector('input[name="read"]:checked').value;
+  console.log(read);
+  for(let i=0;i<myLibrary.length;i++){
+
+    if(max < myLibrary[i].index ){
+      max = myLibrary[i].index; 
+    }
+  }
+  //Index must be > max for the new book
+  index = max+1;
+  newbook = new Book(title,author,pages,read,index);
+  //Add to Array
+  myLibrary.push(newbook);
+
+  //Add to view a book
+  div = document.createElement('div');
+  div.classList.add('book'+this.index); 
+  container.appendChild(div);
+
+  //Add Info about book
+  divinfo = document.createElement('div');
+  divinfo.classList.add('title'+this.index);
+  divinfo.textContent = "Title:"+this.title;
+  div.appendChild(divinfo); 
+
+  divinfo = document.createElement('div');
+  divinfo.classList.add('author'+this.index);
+  divinfo.textContent = "Author:"+this.author;
+  div.appendChild(divinfo); 
+
+  divinfo = document.createElement('div');
+  divinfo.classList.add('pages'+this.index);
+  divinfo.textContent = "Pages:"+this.pages;
+  div.appendChild(divinfo); 
+
+  divinfo = document.createElement('div');
+  divinfo.classList.add('read'+this.index);
+  divinfo.textContent = "Read:"+this.read;
+  div.appendChild(divinfo);
+  
+  //Button Delate Book
+  button = document. createElement('button');
+  button.type = 'button';
+  button.classList.add('button'+this.index);
+  button.setAttribute("id", "book"+this.index); 
+  button.innerText = 'Delete';
+  div.appendChild(button);
+
+  //Button Edit Book
+  button = document. createElement('button');
+  button.type = 'button';
+  button.setAttribute("id", "edit"+this.index); 
+  button.innerText = 'Edit Status';
+  div.appendChild(button);
+}
+
+function RemoveBook(index){
+  //Remove Book From Array
+  for(let i=0;i<myLibrary.length;i++){
+    if(myLibrary[i].index === index){
+      myLibrary.splice(index,index+1);
+    }
+  }
+  //Remove Book from DOM
+  div = document.querySelector('.book'+index);
+  div.remove();
+}
+   
+       
+function listenClicks() {
+      document.addEventListener('click', (event) => {
+        const { target } = event;
+        if (target.id.includes('book')) {
+          index = (target.id).substr(-1);
+          RemoveBook(index); 
+        }
+        else if(target.id === 'btn_add'){
+          addBookToLibrary();
+          document.getElementById("form").reset();
+        }
+        else if(target.id.includes('edit')) {
+          index = (target.id).substr(-1);
+          EditStatus(index);
+          
+        }
+      });
+}
+
+    
+
+//addBookToLibrary.prototype = Object.create(Book.prototype);
 
 
 window.onload = () => {
-    
+
+  if(myLibrary.length > 0)
     inicial(myLibrary);
-    books = document.querySelectorAll(".container > div");
-    buttons_delete = document.querySelectorAll(".container > div > button");
-    //console.log(buttons_delete);
-    buttons_delete.forEach((button) => {
-
-      button.addEventListener('click', () => {
-
-        index = (button.className).substr(-1);
-        //console.log(index);
-        div = document.querySelector('.book'+index);
-        //console.log(div);
-        myLibrary.splice(index,index+1);
-        div.remove();
-        //console.log(myLibrary);
-
-      });
-
-    });
-   
   }
-
- 
-function delete_grid(){
-  books.forEach((div) => {
-    div.remove();
-    //console.log(div);
-  });
-}
+  listenClicks();
